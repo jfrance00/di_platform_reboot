9d805e114c8b71ba408043ff74ec4f95e73c8a4d
jfrance00 <jfrance00@gmail.com>
2020-07-20 15:15:37 +0300

    Added for loops to print number of cards

--- di_app/views.py
+++ di_app/views.py
@@ -19,17 +19,30 @@ def courses():
 
 @app.route('/course/weeks') #TODO course will be turned into a variable to pull relevant data
 def weeks():
-    return flask.render_template('weeks.html')
+    course = {              # !Temporary! data will come from database
+        'length_in_weeks': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        'days_of_week': [1, 2, 3, 4, 5],
+    }
+    return flask.render_template('weeks.html', course=course)
 
 
 @app.route('/course/weeknum/days')  #TODO course variable
 def days():
-    return flask.render_template('days.html')
+    course = {              # !Temporary! data will come from database
+        'length_in_weeks': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        'days_of_week': [1, 2, 3, 4, 5],
+    }
+    return flask.render_template('days.html', course=course)
 
 
 @app.route('/course/weeknum/daynum')  #TODO course variable and day #
 def lesson():
-    return flask.render_template('lesson.html')
+    course = {  # !Temporary! data will come from database
+        'length_in_weeks': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        'days_of_week': [1, 2, 3, 4, 5],
+        'lesson_activities': ['Lecture1', 'Lecture2', 'xp', 'xp gold', 'xp ninja', 'Daily']
+    }
+    return flask.render_template('lesson.html', course=course)
 
 
 @app.route('/course/daynum/resource')  #TODO course variable, day#, resource all variables
